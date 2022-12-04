be647eb3f6bb3a722ed1a1e35193ad35c21c953d
Gert Jan Schoneveld <gj_schoneveld@ymail.com>
2022-12-04 13:29:25 +0100

    Day 4

---
+++ Day04/Program.cs
@@ -0,0 +1,35 @@
+﻿var input = File.ReadAllLines("input.txt");
+var pairs = input.Select(Section.Parse).ToList();
+
+var answer1 = pairs.Count(pair => pair[0].FullyContains(pair[1]) || pair[1].FullyContains(pair[0]));
+Console.WriteLine($"Answer 1: {answer1}");
+
+var answer2 = pairs.Count(pair => pair[0].SomeOverlap(pair[1]));
+Console.WriteLine($"Answer 2: {answer2}");
+
+class Section
+{
+    public int Start { get; set; }
+    public int End { get; set; }
+
+    public bool FullyContains(Section other)
+    {
+        return Start <= other.Start && End >= other.End;
+    }
+
+    public bool SomeOverlap(Section other)
+    {
+        return Start <= other.End && End >= other.Start;
+    }
+
+    public static List<Section> Parse(string line)
+    {
+        var parts = line.Split(',', '-');
+
+        return parts
+            .Select(int.Parse)
+            .Chunk(2)
+            .Select(v => new Section { Start = v[0], End = v[1] })
+            .ToList();
+    }
+}
